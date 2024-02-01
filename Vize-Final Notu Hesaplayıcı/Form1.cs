8bd3654594f82c0bc25ff79e24b3f5a9353fb14c
EnesSiracDev <esiractopaloglu.dev@gmail.com>
2024-02-01 22:26:14 +0300

    Add project files.

---
+++ Vize-Final Notu Hesaplayıcı/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vize_Final_Notu_Hesaplayıcı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ortalama ort = new Ortalama();
+            ort.vize = int.Parse(textBox1.Text);
+            ort.final = int.Parse(textBox2.Text);
+
+            if (ort.Ortalamahesapla() >= 60)
+            {
+                MessageBox.Show("Geçtiniz. Ortalamanız: " + ort.Ortalamahesapla());
+            }
+            else
+            {
+                MessageBox.Show("Kaldınız. Ortalamanız: " + ort.Ortalamahesapla());
+            }
+        }
+    }
+}
---
+++ Vize-Final Notu Hesaplayıcı/Ortalama.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vize_Final_Notu_Hesaplayıcı
+{
+    internal class Ortalama
+    {
+        int _vize;
+        int _final;
+
+        public int vize
+        {
+            get { return _vize; }
+            set
+            {
+                if (value > 100)
+                {
+                    _vize = 100;
+                }
+                else if (value <= 100 && value >= 0)
+                {
+                    _vize = value;
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz bir vize notu girdiniz.");
+                }
+            }
+        }
+
+        public int final
+        {
+            get { return _final; }
+            set
+            {
+                if (value > 100)
+                {
+                    _final = 100;
+                }
+                else if (value <= 100 && value >= 0)
+                {
+                    _final = value;
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz bir final notu girdiniz.");
+                }
+            }
+        }
+
+        public double Ortalamahesapla()
+        {
+            return _vize * 0.4 + _final * 0.6;
+        }
+    }
+}
